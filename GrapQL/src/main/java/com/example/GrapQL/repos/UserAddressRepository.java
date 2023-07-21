8bc392d581dc53c08a7a83edd71cad58b44cac08
dodangnguyen96 <dodangnguyen961997@gmail.com>
2023-07-21 16:38:44 +0700

    init

--- GrapQL/src/main/java/com/example/GrapQL/repos/UserAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.GrapQL.repos;
-
-import com.example.GrapQL.models.UserAddress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAddressRepository extends JpaRepository<UserAddress, Long> {
-    UserAddress findById(long String);
-}

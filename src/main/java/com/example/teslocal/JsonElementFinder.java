fa9aea27f3fa0938d5071b08b7bd2f07335a6260
dodangnguyen96 <dodangnguyen961997@gmail.com>
2023-08-02 11:18:04 +0700

    JsonElementFinder

--- src/main/java/com/example/teslocal/JsonElementFinder.java
+++ src/main/java/com/example/teslocal/JsonElementFinder.java
@@ -4,21 +4,26 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class JsonElementFinder {
 
     public static void main(String[] args) {
-        String jsonString = "[{\"id\":1,\"name\":\"John\"}, {\"id\":2,\"name\":\"Alice\"}, {\"id\":3,\"name\":\"Bob\"}]";
-        int targetId = 2;
+        String jsonString = "[{\"medication\":{\"medicationID\":1,\"drugname\":\"a\",\"ndc:\":\"1223214\"}},{\"medication\":{\"medicationID\":2,\"drugname\":\"b\",\"ndc:\":\"454353\"}},{\"medication\":{\"medicationID\":3,\"drugname\":\"b\",\"ndc:\":\"34324\"}}]";
 
         try {
             JSONArray jsonArray = new JSONArray(jsonString);
 
-            JSONObject foundObject = findElementByFieldValue(jsonArray, "id", targetId);
+            int targetMedicationID = 2;
+
+            JSONObject foundObject = findElementByFieldValue(jsonArray, "medicationID", targetMedicationID);
 
             if (foundObject != null) {
                 System.out.println("Found object: " + foundObject.toString());
             } else {
-                System.out.println("Element with id " + targetId + " not found.");
+                System.out.println("Element with medicationID " + targetMedicationID + " not found.");
             }
 
         } catch (JSONException e) {
@@ -30,7 +35,7 @@ public class JsonElementFinder {
         for (int i = 0; i < jsonArray.length(); i++) {
             try {
                 JSONObject jsonObject = jsonArray.getJSONObject(i);
-                if (jsonObject.has(fieldName) && jsonObject.getInt(fieldName) == fieldValue) {
+                if (jsonObject.has("medication") && jsonObject.getJSONObject("medication").has(fieldName) && jsonObject.getJSONObject("medication").getInt(fieldName) == fieldValue) {
                     return jsonObject;
                 }
             } catch (JSONException e) {
@@ -40,3 +45,4 @@ public class JsonElementFinder {
         return null; // Element not found
     }
 }
+
